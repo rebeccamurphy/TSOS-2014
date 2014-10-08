5c98bec407fbe3dd7f141b0e9d9b7ae9c2fd92f6
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-10-07 20:33:43 -0400

    still starting

---
+++ dist/scripts/os/memoryManager.js
@@ -0,0 +1,12 @@
+/*
+FUTURE ME PUT HELLA SWEET COMMENTS HERE.
+*/
+var TSOS;
+(function (TSOS) {
+    var MemoryManager = (function () {
+        function MemoryManager() {
+        }
+        return MemoryManager;
+    })();
+    TSOS.MemoryManager = MemoryManager;
+})(TSOS || (TSOS = {}));
---
+++ source/scripts/os/memoryManager.ts
@@ -0,0 +1,9 @@
+/*
+FUTURE ME PUT HELLA SWEET COMMENTS HERE. 
+*/
+
+module TSOS {
+    export class MemoryManager {
+        
+    }
+}
